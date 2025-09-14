fed424a6cf8f672d32fe503b753ac820640bcdae
santiago velazquez <santiagovdiaz@outlook.com>
2025-09-14 19:46:29 -0400

    add dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import PageDashboard from "./dashboard/page";
+import OfficeCard from "./office-card";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-50">
-      <h1 className="text-4xl font-bold text-blue-600">Deskly</h1>
-    </main>
-  )
+    <>
+
+      <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 gap-8">
+        {/* <PageDashboard /> */}
+        <h1 className="text-4xl font-bold text-blue-600">Deskly!</h1>
+
+        <OfficeCard />
+      </main>
+    </>
+  );
 }
